498b80629d0727f1961c3a13eb4387c1849758f0
MirBarnes <134976426+MirBarnes@users.noreply.github.com>
2024-04-09 16:44:29 -0400

    Enabling navigation between pages
    Now there is navigation. Not very pretty and code is probably kind of messy.

--- db-app/src/App.js
+++ db-app/src/App.js
@@ -1,13 +1,61 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./pages/App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/homepage";
+import Storm from "./pages/stormpage.js";
+import TempDeath from "./pages/temppage.js";
+import InsectAg from "./pages/invasivepage.js";
+import Precip from "./pages/precippage.js";
+import GlobSpend from "./pages/globspendpage.js";
 
 function App() {
   return (
-    <div className="App">
+    <>
+    {/* This is the alias of BrowserRouter i.e. Router */}
+    <Router>
+        <Routes>
+            {/* This route is for home component 
+  with exact path "/", in component props 
+  we passes the imported component*/}
+            <Route
+                exact
+                path="/"
+                element={<Home />}
+            />
+            <Route
+                path="/storm"
+                element={<Storm />}
+            />
+            <Route
+                path="/tempdeath"
+                element={<TempDeath />}
+            />
+            <Route
+                path="/insectag"
+                element={<InsectAg />}
+            />
+            <Route
+                path="/precip"
+                element={<Precip />}
+            />
+            <Route
+                path="/globspend"
+                element={<GlobSpend />}
+            />
+        </Routes>
+    </Router>
+</>
+);
+    /* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,7 +67,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  ); */
 }
 
 export default App;
---
+++ db-app/src/pages/stormpage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+// Importing Link from react-router-dom to 
+// navigate to different end points.
+import { Link } from "react-router-dom";
+ 
+const Storm = () => {
+    return (
+        <div>
+            <header className="App-header"> 
+            <h1>Storm Data Page</h1>
+            <ul>
+                <li className="ul_top_hypers">
+                    {/* Endpoint to route to Home component */}
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="ul_top_hypers">
+                    {/* Endpoint to route to Storm component */}
+                    <Link to="/storm">Storm</Link>
+                </li>
+                <li className="ul_top_hypers">
+                    {/* Endpoint to route to TempDeath component */}
+                    <Link to="/tempdeath">Temperature-Dependent Mortality</Link>
+                </li>
+                <li className="ul_top_hypers">
+                    {/* Endpoint to route to InsectAg component */}
+                    <Link to="/insectag">Invasive Insects</Link>
+                </li>
+                <li className="ul_top_hypers">
+                    {/* Endpoint to route to Precip component */}
+                    <Link to="/precip">Extreme Precipitation</Link>
+                </li>
+                <li className="ul_top_hypers">
+                    {/* Endpoint to route to GlobSpend component */}
+                    <Link to="/globspend">Global Spending</Link>
+                </li>
+            </ul>
+            </header>
+            <br />
+        </div>
+    );
+};
+ 
+export default Storm;
