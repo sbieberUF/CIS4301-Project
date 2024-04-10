5296bb8216f8f23a4a5514fddf72656e11272c31
Sydney Bieber <sbieber@ufl.edu>
2024-04-09 20:36:37 -0400

    webpage outline

--- db-app/src/App.js
+++ db-app/src/App.js
@@ -1,25 +1,63 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("Overview");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Exploring Environmental Resilience and Response</h1>
       </header>
+      <div className="tabs">
+        {["Overview", "Flight Delays", "Temperature-Dependent Mortality", "Invasive Insects", "Housing Index", "Government Spending"].map(tab => (
+          <div
+            key={tab}
+            className={`tab ${activeTab === tab ? "active" : ""}`}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab}
+          </div>
+        ))}
+      </div>
+      <div className="content">
+        {activeTab === "Overview" && <OverviewPage/>}
+        {activeTab === "Flight Delays" && <FlightDelaysPage/>}
+        {activeTab === "Temperature-Dependent Mortality" && <TemperatureDependentMortalityPage/>}
+        {activeTab === "Invasive Insects" && <InvasiveInsects/>}
+        {activeTab === "Housing Index" && <HousingIndex/>}
+        {activeTab === "Government Spending" && <GovernmentSpending/>}
+      </div>
     </div>
   );
+}
+
+function OverviewPage() {
+  return <div>Overview Page</div>;
+}
+
+function FlightDelaysPage() {
+  return <div>Flight Delays Page</div>;
+}
+
+function TemperatureDependentMortalityPage() {
+  return <div>Temperature-Dependent Mortality</div>;
+}
+
+function InvasiveInsects() {
+  return <div>Invasive Insects</div>;
+}
+
+function HousingIndex() {
+  return <div>Housing Index</div>;
+}
+
+function GovernmentSpending() {
+  return <div>Government Spending</div>;
 }
 
 export default App;
