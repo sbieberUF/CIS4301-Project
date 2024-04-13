32cb9854cb674f3d3bb2b625d5340cca601176d9
lnsidiou5 <21jason.li@gmail.com>
2024-04-12 20:32:26 -0400

    Successful custom query for severeWeather tab

--- db-app/src/severeWeather.js
+++ db-app/src/severeWeather.js
@@ -8,10 +8,12 @@ import {
   Legend,
   LineChart,
 } from "recharts";
+import axios from "axios";
 
 function SevereWeatherTab() {
   const [options, setOptions] = useState({
     stateCounty: "ALL",
+    stormEvent: "ALL",
     startDate: "1950-01-01",
     endDate: "2023-12-31",
     resolution: "By Year",
@@ -22,7 +24,10 @@ function SevereWeatherTab() {
   });
   const [data1, setData1] = useState(null);
   // Mock data for right now
+  //fix these two
+  const eventTypes = ["ALL", "Thunderstorm Wind", "Hail"];
   const states = ["ALL", "State1", "State2", "State3"];
+
   const resolution = ["By Year", "By Month"];
   const sex = ["ALL", "M", "F"];
   const fatalityType = ["ALL", "D", "I"];
@@ -65,26 +70,59 @@ function SevereWeatherTab() {
       );
     });
   };
-  const generateGraph = () => {
-    // Logic for generating the graph based on selected data
-    console.log("Generating graph...");
-    //fetch data with the custom query and format it like the below
-    //must be ordered by date, since dates cannot be sorted by recharts
-    var dataParsed = [
-      { date: "2011-07-05", value: 1, oil: 2 },
-      { date: "2021-08-05", value: 2, oil: 1 },
-      { date: "2021-09-05", value: 3, oil: 2 },
-      { date: "2021-10-05", value: 4, oil: 1 },
-      { date: "2021-11-05", value: 5, oil: 2 },
-      { date: "2021-12-05", value: 6, oil: 1 },
-      { date: "2022-01-05", value: 1, oil: 2, gas: 3 },
-      { date: "2022-02-05", value: 2, oil: 1, gas: 1 },
-      { date: "2022-03-05", value: 3, oil: 2, gas: 3 },
-      { date: "2022-04-05", value: 4, oil: 1, gas: 3 },
-      { date: "2022-05-05", value: 5, oil: 2, gas: 7 },
-      { date: "2022-06-05", value: 6, oil: 1, gas: 3 },
-    ];
-    setData1(dataParsed);
+  const generateGraph = async (options) => {
+    //format the string
+    var timeformat = "YYYY-MM";
+    if (options.resolution === "By Year") {
+      timeformat = "YYYY";
+    }
+    var conditions = "";
+    if (options.stateCounty !== "ALL") {
+      if (conditions.length === 0) {
+      }
+      conditions = `WHERE st.SNAME = '${options.stateCounty}'`;
+    }
+    if (options.stormEvent !== "ALL") {
+      if (conditions.length === 0) {
+        conditions = "WHERE ";
+      }
+      conditions = `${conditions}EVENT_TYPE = '${options.stormEvent}'`;
+    }
+    var queryText1 = `WITH dates(yearMonth) AS (
+      SELECT to_char(BEGIN_DATE_TIME,'${timeformat}') FROM "JASON.LI1".STORM_EVENT 
+      WHERE BEGIN_DATE_TIME >= to_timestamp('${options.startDate}', 'YYYY-MM-DD')
+      AND BEGIN_DATE_TIME <= to_timestamp('${options.endDate}', 'YYYY-MM-DD')
+      GROUP BY to_char(BEGIN_DATE_TIME,'${timeformat}')
+  ),
+  eventList(yearMonth) AS (
+      SELECT to_char(BEGIN_DATE_TIME,'${timeformat}') FROM "JASON.LI1".STORM_EVENT se
+      INNER JOIN "JASON.LI1".STORM_LOC sl ON se.EVENT_ID = sl.EVENT_ID
+      INNER JOIN "JASON.LI1".STATES st ON st.FIPS = sl.STATE_FIPS
+      ${conditions}
+  )
+  SELECT dates.yearMonth, COUNT(eventList.yearMonth) FROM dates 
+  LEFT JOIN eventList ON eventList.yearMonth = dates.yearMonth 
+  GROUP BY dates.yearMonth ORDER BY dates.yearMonth`;
+    console.log(queryText1);
+    axios
+      .get(`http://localhost:5001/?query=${encodeURIComponent(queryText1)}`, {
+        crossdomain: true,
+      })
+      .then((response) => {
+        console.log(response.data);
+        // Logic for generating the graph based on selected data
+        console.log("Generating graph...");
+        //fetch data with the custom query and format it like the below
+        //must be ordered by date, since dates cannot be sorted by recharts
+        var dataParsed = [];
+        for (let i in response.data) {
+          dataParsed.push({
+            date: response.data[i][0],
+            eventFrequency: response.data[i][1],
+          });
+        }
+        setData1(dataParsed);
+      });
   };
 
   return (
@@ -92,6 +130,21 @@ function SevereWeatherTab() {
       <h2>Select Criteria</h2>
       <div>
         <h3>General</h3>
+        <label>
+          Select Event Type:
+          <select
+            name="stormEvent"
+            value={options.stormEvent}
+            onChange={handleChange}
+          >
+            {eventTypes.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
         <label>
           Select State:
           <select
@@ -209,7 +262,13 @@ function SevereWeatherTab() {
         </label>
       </div>
       <hr style={{ margin: "20px 0" }} />
-      <button onClick={generateGraph}>Generate Graphs</button>
+      <button
+        onClick={() => {
+          generateGraph(options);
+        }}
+      >
+        Generate Graphs
+      </button>
       {data1 && (
         <LineChart
           width={500}
@@ -224,7 +283,7 @@ function SevereWeatherTab() {
         >
           <CartesianGrid stroke="#f5f5f5" />
           <XAxis dataKey="date" />
-          <YAxis />
+          <YAxis domain={["dataMin", "dataMax"]} />
           <Tooltip />
           <Legend
             layout="vertical"
